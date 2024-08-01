b5efd318692cb11c95c508898b5cf191f15947c4
Oliver Malate <oliver95malate@gmail.com>
2024-08-02 03:36:48 +0800

    show mobile menu and sub menu

--- script.js
+++ script.js
@@ -17,3 +17,29 @@ function copyMenu() {
 }
 
 copyMenu();
+
+// show mobile menu
+const menuButton = document.querySelector(".trigger"),
+  closeButton = document.querySelector(".t-close"),
+  addClass = document.querySelector(".site");
+
+menuButton.addEventListener("click", function () {
+  addClass.classList.toggle("showmenu");
+});
+
+closeButton.addEventListener("click", function () {
+  addClass.classList.remove("showmenu");
+});
+
+// show sub menu on mobile
+const submenu = document.querySelectorAll(".has-child .icon-small");
+submenu.forEach((menu) => menu.addEventListener("click", toggle));
+
+function toggle(e) {
+  e.preventDefault();
+  submenu.forEach((item) =>
+    item != this ? item.closest(".has-child").classList.remove("expand") : null
+  );
+  if (this.closest(".has-child").classList != "expand");
+  this.closest(".has-child").classList.toggle("expand");
+}
