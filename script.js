371e89969afcd009a331547999136e704ac2a1d5
Oliver Malate <oliver95malate@gmail.com>
2024-08-03 02:13:13 +0800

    added swiper fw to slider

--- script.js
+++ script.js
@@ -43,3 +43,12 @@ function toggle(e) {
   if (this.closest(".has-child").classList != "expand");
   this.closest(".has-child").classList.toggle("expand");
 }
+
+// slider
+const swiper = new Swiper(".swiper", {
+  loop: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
